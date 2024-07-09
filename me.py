2a702cc451b97751fa0c8c05b22580dfa2da07d3
ChuksTech <chukzdigitalz@gmail.com>
2024-07-09 05:15:12 -0700

    first Python code

---
+++ me.py
@@ -0,0 +1,21 @@
+print("Hello, greetings from ChuksTech")
+a=100
+print(a)
+a="Coming"
+print(a)
+ABS="Apple"
+print(ABS)
+d, t, y = "Dotun", 20, "Ade"
+print(d, t, y)
+is_student= False
+print(is_student)
+result = 10+10
+print(result)
+product = 7 - 5
+print(product)
+quotient = 20/4
+print(quotient)
+power = 2 ** 3
+print (power)
+power = 2 * 3
+print(power)
